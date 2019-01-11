6d515545da93c594d17506fdadabddc6c9f3392f
krisrybak <kris.rybak@hotmail.co.uk>
2019-01-11 13:08:38 +0000

    Adds new error code to deal with organisation mismatch

--- Exception/ErrorCode.php
+++ Exception/ErrorCode.php
@@ -17,6 +17,7 @@ class ErrorCode
     const USER_EMAIL_PRIMARY_MISSING    = 4040021;
     const USER_TOKEN_MALFORMED          = 4030001;
     const USER_TOKEN_INVALID            = 4030002;
+    const USER_ORGANISATION_MISMATCH    = 4030003;
     const PASSWORD_RESET_LIMIT_EXCEEDED = 4290001;
     const PASSWORD_TOKEN_INVALID        = 4040031;
 
@@ -25,6 +26,7 @@ class ErrorCode
         4010002 => 'Authentication Required',
         4030001 => 'Malformed user token. User token could not be parsed',
         4030002 => 'Invalid user token. User token is not valid',
+        4030003 => 'User does not belong to given Organisation',
         4040001 => 'User not found. Supplied username or email address is invalid',
         4040021 => 'No primary email has been defined for the user',
         4040031 => 'Password token is not valid',
