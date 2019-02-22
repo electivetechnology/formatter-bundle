0066fe8955f99da3d8fa5c06a5cc67dc7b4afb54
krisrybak <kris.rybak@hotmail.co.uk>
2019-02-22 12:44:29 +0000

    Adds Tests for cacheable

--- Tests/Cache/Triats/CacheableTest.php
+++ Tests/Cache/Triats/CacheableTest.php
@@ -10,9 +10,15 @@ class A {
     use Cacheable;
 }
 
-class B implements CacheInterface{
+class Adapter implements CacheInterface{
     public function get(string $key, callable $callback, float $beta = null, array &$metadata = null){}
     public function delete(string $key): bool {}
+    public function getItem(string $key) {}
+}
+
+class Item {
+    public function isHit(): bool {}
+    public function get() {}
 }
 
 /**
@@ -24,9 +30,47 @@ class CacheableTest extends TestCase
 {
     public function testSetCacheAdapter() {
         $a = new A;
-        $b = new B;
+        $b = new Adapter;
         $this->assertInstanceOf(A::class, $a->setCacheAdapter($b));
         $this->assertInstanceOf(CacheInterface::class, $a->getCacheAdapter());
         $this->assertSame($b, $a->getCacheAdapter());
+    }
+
+    public function testGetCacheItem() {
+        $data = new \StdClass;
+        $data->foo = "bar";
+
+        $nonExistingItem = $this->createMock(Item::class);
+
+        // Configure the stub.
+        $nonExistingItem->method('isHit')
+             ->willReturn(false);
+
+        $stub = $this->createMock(Adapter::class);
+
+        // Configure the stub.
+        $stub->method('getItem')
+             ->willReturn($nonExistingItem);
+
+        $a = new A;
+        $this->assertInstanceOf(A::class, $a->setCacheAdapter($stub));
+        $this->assertFalse($a->getCacheItem("key"));
+
+        $existingItem = $this->createMock(Item::class);
+
+        // Configure the stub.
+        $existingItem->method('isHit')
+            ->willReturn(true);
+        $existingItem->method('get')
+            ->willReturn(serialize($data));
+
+        // Configure the stub.
+        $adapter = $this->createMock(Adapter::class);
+        $adapter->method('getItem')
+             ->willReturn($existingItem);
+
+        $aa = new A;
+        $this->assertInstanceOf(A::class, $aa->setCacheAdapter($adapter));
+        $this->assertEquals($data, $aa->getCacheItem("key"));
     }
 }
