e067f599e3d8f3326e92ada8286bef6c3fbb4b7f
krisrybak <kris.rybak@hotmail.co.uk>
2019-03-06 17:30:20 +0000

    Adds Exception Listener

---
+++ Listener/Exception.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Elective\FormatterBundle\Listener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Elective\FormatterBundle\Response\FormatterInterface;
+use Elective\FormatterBundle\Exception\ApiException;
+
+/**
+ * Elective\FormatterBundle\Listener\Exception
+ *
+ * @author Kris Rybak <kris.rybak@electivegroup.com>
+ *
+ */
+class Exception implements EventSubscriberInterface
+{
+    /**
+     * @var FormatterInterface
+     */
+    private $formatter;
+
+    /**
+     * @var string
+     */
+    private $env;
+
+    public function __construct(FormatterInterface $formatter, $env = 'prod')
+    {
+        $this->formatter = $formatter;
+        $this->env = $env;
+    }
+
+    /**
+     * Get Formatter
+     *
+     * @return FormatterInterface
+     */
+    public function getFormatter(): FormatterInterface
+    {
+        return $this->formatter;
+    }
+
+    /**
+     * Set Formatter
+     *
+     * @param $formatter FormatterInterface
+     * @return self
+     */
+    public function setFormatter(FormatterInterface $formatter): self
+    {
+        $this->formatter = $formatter;
+
+        return $this;
+    }
+
+    /**
+     * Get Env
+     *
+     * @return string
+     */
+    public function getEnv(): string
+    {
+        return $this->env;
+    }
+
+    /**
+     * Set Env
+     *
+     * @param $env string
+     * @return self
+     */
+    public function setEnv(string $env): self
+    {
+        $this->env = $env;
+
+        return $this;
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        // Return symfony flavour exception in dev mode for all system exceptions
+        if ($this->getEnv() != 'dev' || $event->getException() instanceof ApiException) {
+            $ret            = new \StdClass;
+            $ret->message   = $event->getException()->getMessage();
+            $ret->code      = $event->getException()->getCode();
+
+            // Check if the exception is HTTP Exception type and overwrite defaults
+            if (method_exists($event->getException(), "getStatusCode")) {
+                $statusCode = $event->getException()->getStatusCode();
+            } else {
+                $statusCode = JsonResponse::HTTP_INTERNAL_SERVER_ERROR;
+            }
+
+            if ($event->getException() instanceof ApiException) {
+                $this->formatter->setHeaders($event->getException()->getHeaders());
+            }
+
+            $response = $this->formatter->render($ret, $statusCode);
+            $event->setResponse($response);
+        }
+
+        return $event;
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::EXCEPTION => 'onKernelException',
+        );
+    }
+}
---
+++ Tests/Listener/ExceptionTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Elective\FormatterBundle\Tests\Listener\Cache;
+
+use Elective\FormatterBundle\Listener\Exception;
+use Elective\FormatterBundle\Response\Formatter;
+use Elective\FormatterBundle\Response\FormatterInterface;
+use PHPUnit\Framework\TestCase;
+use Elective\FormatterBundle\Exception\ApiException;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+/**
+ * Elective\FormatterBundle\Tests\Listener\ExceptionTest
+ * @group listener
+ * @author Kris Rybak <kris@electivegroup.com>
+ */
+class ExceptionTest extends TestCase
+{
+    protected $listener;
+
+    protected function setUp(): void
+    {
+        $formatter = new Formatter();
+        $this->listener = new Exception($formatter);
+    }
+
+    public function testSetGetFormatter()
+    {
+        $formatter = $this->createMock(FormatterInterface::class);
+        $this->assertInstanceOf(Exception::class, $this->listener->setFormatter($formatter));
+        $this->assertSame($formatter, $this->listener->getFormatter());
+    }
+
+    public function envProvider()
+    {
+        return array(
+            array('dev'),
+            array('test'),
+            array('prod'),
+        );
+    }
+
+    /**
+     * @dataProvider envProvider
+     */
+    public function testSetGetEnv($env)
+    {
+        $this->assertInstanceOf(Exception::class, $this->listener->setEnv($env));
+        $this->assertSame($env, $this->listener->getEnv());
+    }
+
+    public function onKernelExceptionProvider()
+    {
+        $data = array();
+        $kernel = $this->createMock(HttpKernelInterface::class);
+        $request = $this->createMock(Request::class);
+
+        $exception = new ApiException('foo', 400000, 400, ['X-foo' => 'bar']);
+        $event = new GetResponseForExceptionEvent($kernel, $request, HttpKernelInterface::MASTER_REQUEST, $exception);
+
+        $data[] = array('dev', $event, true, 400, 'X-foo', 'bar');
+        $data[] = array('test', $event, true, 400, 'X-foo', 'bar');
+        $data[] = array('prod', $event, true, 400, 'X-foo', 'bar');
+
+        $exception = new \Exception('foo');
+        $event = new GetResponseForExceptionEvent($kernel, $request, HttpKernelInterface::MASTER_REQUEST, $exception);
+        $data[] = array('prod', $event, true, 500);
+
+        return $data;
+    }
+
+    /**
+     * @dataProvider onKernelExceptionProvider
+     */
+    public function testOnKernelException($env, $event, $isResponse = false, $expectedStatusCode = null, $headerKey = null, $headerVal = null)
+    {
+        $formatter  = new Formatter();
+        $listener   = new Exception($formatter, $env);
+        $responseEvent = $listener->onKernelException($event);
+        $this->assertInstanceOf(GetResponseForExceptionEvent::class, $responseEvent);
+
+        if ($expectedStatusCode) {
+            $this->assertEquals($expectedStatusCode, $responseEvent->getResponse()->getStatusCode());
+        }
+
+        if ($isResponse) {
+            $this->assertInstanceOf(Response::class, $responseEvent->getResponse());
+        }
+
+        $this->assertEquals($headerVal, $responseEvent->getResponse()->headers->get($headerKey));
+    }
+}
