817ee4539d427e0843c6d0c2e8e02e94ad5ae26e
krisrybak <kris.rybak@hotmail.co.uk>
2019-05-07 17:23:29 +0100

    Adds error code property to exception

--- Model/ModelException.php
+++ Model/ModelException.php
@@ -12,5 +12,26 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class ModelException extends \Exception
 {
+    protected $errorCode;
+
     const MODEL_ERROR = Response::HTTP_INTERNAL_SERVER_ERROR;
+
+    public function __construct($message = '', $code = 0, $errorCode = 0)
+    {
+        parent::__construct($message, $code);
+
+        $this->setErrorCode($errorCode);
+    }
+
+    public function setErrorCode($errorCode): self
+    {
+        $this->errorCode = $errorCode;
+
+        return $this;
+    }
+
+    public function getErrorCode()
+    {
+        return $this->errorCode;
+    }
 }
