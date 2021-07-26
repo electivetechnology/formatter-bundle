c71ece4b8c808c085b69c67709d830712d2b41ba
Kris Rybak <kris.rybak@hotmail.co.uk>
2021-07-26 18:50:17 +0100

    Adds context and wall time

--- Traits/Loggable.php
+++ Traits/Loggable.php
@@ -16,6 +16,46 @@ trait Loggable
      */
     private $logger;
 
+    /**
+     * @var array
+     */
+    private $context;
+
+    /**
+     * @var flat
+     */
+    private $startTime;
+
+    public function setContext($context, $startTime = 0): self
+    {
+        $this->context = $context;
+
+        // Reset startTime time
+        if (!$startTime) {
+            $startTime = microtime(TRUE);
+        }
+        $this->setStartTime($startTime);
+
+        return $this;
+    }
+
+    public function getContext(): array
+    {
+        return $this->context;
+    }
+
+    public function setStartTime($startTime): self
+    {
+        $this->startTime = $startTime;
+
+        return $this;
+    }
+
+    public function getStartTime(): float
+    {
+        return $this->startTime;
+    }
+
     public function setLogger($logger): self
     {
         $this->logger = $logger;
@@ -29,12 +69,30 @@ trait Loggable
     }
 
     public function debug($message, array $context = array()) {
-        $utime = sprintf('%.4f', microtime(TRUE));
+        $mtime = microtime(TRUE);
+        $utime = sprintf('%.4f', $mtime);
         $raw_time = \DateTime::createFromFormat('U.u', $utime);
         $now = $raw_time->format('Y-m-d H:i:s.u');
-        $uuid = (isset($context['uuid'])) ? $context['uuid'] : (string) rand(100,999);
+
+        // Override context
+        if ($context) {
+            $this->setContext($context, $mtime);
+        }
+
+        $uuid = (isset($this->getContext()['uuid'])) ? $this->getContext()['uuid'] : (string) rand(100,999);
         $message = sprintf(">>[%s] [%s]: %s", $uuid, $now, $message);
 
-        $this->getLogger()->debug($message, $context);
+        $this->getLogger()->debug($message, $this->getContext());
+    }
+
+    public function debugContext() {
+        // Current time
+        $mtime = microtime(TRUE);
+
+        // Difference
+        $diff = $mtime - $this->getStartTime();
+
+        $message = sprintf("Request took %d ms to process", $diff *1000);
+        $this->debug($message);
     }
 }
