19d1466b4e31c1059d9bb094538dc52c592399af
Kris Rybak <kris.rybak@hotmail.co.uk>
2021-07-30 11:31:37 +0100

    Logger interface extends psr logger interface

--- Logger/RequestLoggerInterface.php
+++ Logger/RequestLoggerInterface.php
@@ -2,28 +2,14 @@
 
 namespace Elective\FormatterBundle\Logger;
 
+use Psr\Log\LoggerInterface;
+
 /**
  * Elective\FormatterBundle\Logger\RequestLoggerInterface
  *
  * @author Kris Rybak <kris.rybak@krisrybak.com>
  */
-interface RequestLoggerInterface
+interface RequestLoggerInterface extends LoggerInterface
 {
     public function log($level, $message, array $context = array());
-
-    public function info($message, array $context = array());
-
-    public function debug($message, array $context = array());
-
-    public function critical($message, array $context = array());
-
-    public function error($message, array $context = array());
-
-    public function warning($message, array $context = array());
-
-    public function notice($message, array $context = array());
-
-    public function emergency($message, array $context = array());
-
-    public function alert($message, array $context = array());
 }
